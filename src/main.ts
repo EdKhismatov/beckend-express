12171a0a0040a5657a3ce791e0e93741130b7084
EduardKhismatov <E.Khismatov@yandex.ru>
2025-11-29 22:13:30 +0500

    fix:Add methods

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ import express from 'express';
 const server = express();
 
 const port = 2000;
+server.use(express.json()); // Включаем парсер тела
 
-server.get('/hello', (req, res) => {
-  console.log('Использую метод DELETE');
-  res.send('Привет');
+server.get('/payload/:id', (req, res) => {
+  console.log('Использую метод POST');
+  console.log(req.params);
+  res.send(`Вернул парамс ${req.params}`);
+});
+
+server.get('/payload/user', (req, res) => {
+  console.log('Использую метод POST');
+  console.log(req.query);
+  res.send(`Вернул query`);
+});
+
+server.post('/payload', (req, res) => {
+  console.log('Использую метод POST');
+  console.log(req.body);
+  res.send(`Вернул body`);
 });
 
 server.listen(port, () => {
