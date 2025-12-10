4cea9243ee22cd5a3adb94fd812b75abd0cf9d9d
EduardKhismatov <E.Khismatov@yandex.ru>
2025-12-11 02:09:13 +0500

    fix: oop

--- src/mocks/user.mocks.ts
+++ src/mocks/user.mocks.ts
@@ -4,12 +4,22 @@ export type UserMock = {
   id: number;
   email: string;
   name: string;
+  password?: string;
 };
 
-export const userMock = (): UserMock => {
+export const userLoginMock = (): UserMock => {
   return {
     id: faker.number.int({ min: 1, max: 1000 }),
     email: faker.internet.email(),
     name: faker.person.fullName(),
+  };
+};
+
+export const userRegMock = (): UserMock => {
+  return {
+    id: faker.number.int({ min: 1, max: 1000 }),
+    email: faker.internet.email(),
+    name: faker.person.fullName(),
+    password: faker.string.alpha(10),
   };
 };
--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import logger from '../logger/pino.logger';
-import { userMock } from '../mocks/user.mocks';
+import { userLoginMock, userRegMock } from '../mocks/user.mocks';
 
 export const userRouter = express.Router();
 
 userRouter.post('/register', (req, res) => {
   logger.info('Сгенерирован новый пользователь');
-  res.json(userMock());
+  res.json(userRegMock());
 });
 
 userRouter.post('/login', (req, res) => {
   logger.info('Логин пользователя');
-  res.json(userMock());
+  res.json(userLoginMock());
 });
